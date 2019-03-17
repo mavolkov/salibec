55f8f1ae9cae1e7577672a31b97fef45a8fdd5fc
Mikhail Volkov <misha6213501@mail.ru>
2019-03-17 22:28:59 +0300

    Update 5string1.cpp

--- 5string1.cpp
+++ 5string1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iostream>
 #include <map>
 #include <iostream>
 #include <vector>
@@ -22,17 +21,49 @@ int main() {
     //vector<string> lines;
     //string line;
 
+//----------------------------------------------------------------------------------------
+
     cout<<"\n";
     cout<<"Строка до:";
-    string s1 = "Hello abrams abbyy kiabk by about abroad it";
-    cout<<"\n"<<s1<<"\n\n";
+    string s1 = "Hello abrams abbyy kiabk by about z abroad w i it amb above";
+    cout<<"\n"<<s1<<"\n";
 
-    cout<<"Заменяем ab на ссс: ";
+    cout<<"1) Заменяем ab на ссс: ";
     while(s1.find("ab") < s1.size())
         s1 = s1.substr(0,s1.find("ab")) + "ccc" + s1.substr(s1.find("ab")+2,s1.size());
     cout<<"\n"<<s1<<"\n\n";
 
-    cout<<"По 2 конечные буквы:\n";
+//----------------------------------------------------------------------------------------
+
+    cout<<"2) По 2 конечные буквы:\n";
+    string s2 = "";
+    for (int i=0; i<s1.size(); i++){
+        if (s1.substr(i,1)==" ") { //если пробел
+            string temp = s1.substr(i - 2, 2);
+            if (temp.find(" ") != 0) { //если нет пробела(длина > 1 символа)
+                cout << temp << " ";
+                s2 += temp;
+            }
+        }
+    }
+    cout<<"\n";
+    cout<<"Подстрока из них:\n";
+    cout<< s2 << "\n";
+
+//----------------------------------------------------------------------------------------
+
+    cout<<"\n3) Предложение из тех слов массива, которые не содержат в своем составе символов из подстроки.\n";
+    string s3 = "";
+    for (int i=1; i<s1.size()-1; i++){
+        //cout<< s1[i-1] << " - " << s1[i+1] << "\n";
+        if (s1[i-1] == *" " && s1[i+1] == *" "){
+            //cout<<s1[i]<<"\n";
+            s3 = s3 + s1[i] + " ";
+        }
+    }
+    cout<< s3 << "\n";
+
+
 
     return 0;
 }
